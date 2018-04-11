dfc26a3e2c687f1169f2922df807de650daaba35
s1mple <798342418@qq.com>
2018-04-11 13:32:49 +0800

    BI-203 - zhangzhen - 删除不需要的静态文件渲染的相关依赖

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const Koa = require('koa')
 
 const app = new Koa()
-const views = require('koa-views')
 const json = require('koa-json')
 const onError = require('koa-onerror')
 const bodyParser = require('koa-bodyparser')
@@ -21,11 +20,6 @@ app.use(bodyParser({
 }))
 app.use(json())
 app.use(logger())
-app.use(require('koa-static')(`${__dirname}/public`))
-
-app.use(views(`${__dirname}/views`, {
-  extension: 'pug'
-}))
 
 // connect to mongoDB
 app.init = async () => {
