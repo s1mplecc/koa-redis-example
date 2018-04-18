19ff3181c0763fb4158e98507cad0e630a273b98
s1mple <798342418@qq.com>
2018-04-18 14:43:01 +0800

    BI-184 - zhangzhen - 修改 /login 接口，加入 bcrypt 密码盐加密

--- controllers/login.js
+++ controllers/login.js
@@ -2,14 +2,18 @@ const router = require('koa-router')()
 const db = require('../config/mongo')
 const client = require('../config/redis')
 const UUID = require('uuid-js')
+const bcrypt = require('bcrypt')
 
 /**
- *
+ * Check user login & generate access_token
+ * set token and userId hash to redis
+ * @code {String} hash: the data encrypted with salt
  */
 const login = async (ctx) => {
   const { username, password } = ctx.request.body
   const user = await db.user.findOne({ username })
-  ctx.assert(user.password === password, '401', 'Incorrect password!')
+  const hash = bcrypt.hashSync(password, user.salt)
+  ctx.assert(hash === user.password, '401', 'Incorrect password!')
   const token = UUID.create().toString()
   await client.hmset('access_token', token, user.userId)
   ctx.body = {
