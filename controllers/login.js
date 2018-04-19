79af917531d562e349e7b4726def3b9bb4f77087
s1mple <798342418@qq.com>
2018-04-19 16:31:44 +0800

    BI-184 - zhangzhen - 设置 token 过期时间为 3600s

--- controllers/login.js
+++ controllers/login.js
@@ -1,8 +1,11 @@
 const router = require('koa-router')()
 const db = require('../config/mongo')
 const redis = require('../config/redis')
-const UUID = require('uuid-js')
 const bcrypt = require('bcryptjs')
+const TokenGen = require('uuid-token-generator')
+
+const tokenGen = new TokenGen(256, TokenGen.BASE62)
+const TOKEN_EXPIRE_TIME = 3600
 
 /**
  * Check user login & generate access_token
@@ -13,8 +16,9 @@ const login = async (ctx) => {
   const user = await db.user.findOne({ username })
   ctx.assert(user, '401', 'User not found!')
   ctx.assert(bcrypt.compareSync(password, user.password), '401', 'Incorrect password!')
-  const token = bcrypt.hashSync(UUID.create().toString())
-  await redis.hmset('access_token', token, user.userId)
+  const token = tokenGen.generate()
+  await redis.set(`access_token:${token}`, user.userId)
+  redis.expire(`access_token:${token}`, TOKEN_EXPIRE_TIME)
   const { email, userId } = user
   ctx.body = {
     access_token: token,
@@ -28,7 +32,7 @@ const login = async (ctx) => {
 const logout = async (ctx) => {
   const token = ctx.request.header.authorization
   console.log('token', token)
-  await redis.hdel('access_token', token)
+  await redis.del(`access_token:${token}`, token)
   ctx.body = 'ok'
 }
 
